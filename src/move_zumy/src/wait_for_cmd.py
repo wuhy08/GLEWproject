8711b3b2a29394b7dc4c616ac8b4479b4447fb73
Haoyu Wu <wuhaoyu1989@gmail.com>
2015-11-24 11:15:29 -0800

    Add moveEnabled property

--- src/move_zumy/src/wait_for_cmd.py
+++ src/move_zumy/src/wait_for_cmd.py
@@ -5,61 +5,55 @@ import math
 from ar_coord.msg import ZumyCoord
 from geometry_msgs.msg import Twist,Transform,TransformStamped
 from tf2_msgs.msg import TFMessage
+from std_msgs.msg import Bool
 from move_zumy.srv import Mov2LocSrv, Mov2LocSrvResponse
 import get_vel
 import config
 
-
+#Creat class MoveZumy, all the publishing, subscribing and Service will happen here
 class MoveZumy:
 	def __init__(self, zumy_name):
 		rospy.init_node('move_zumy'+zumy_name)
 		self.name = zumy_name
 		self.position = ZumyCoord().position
 		self.ARTag = config.zumy_ar_pair[self.name]
+		#Will subscribe to corresponding AR tag postion 
+		#and once get new message, call getPos to update position
 		rospy.Subscriber('/'+self.ARTag+'/AR_position', ZumyCoord, self.getPos)
 		self.rate = rospy.Rate(10)
 		self.goal = self.position
 		self.goal_flag = True
-		self.counter = 0
+		self.goalCounter = 0
+		#moveEnable will enable or disable the movement of Zumy
+		self.moveEnable = True
+		#The permission will be published by a topic that is responsible for coordinating zumys
+		#TODO: may need another property that is responsible for channging zumy's route
+		rospy.Subscriber('/'+self.name+'/MovePermission', Bool, self.updatePermission)
+		# The publisher will be responsible for publishing zumy's velocity
 		self.vel_pub = rospy.Publisher('/%s/cmd_vel' % self.name, Twist, queue_size=2)
-		#self.pos_tf_pub = rospy.Publisher('/tf', TFMessage,queue_size=2)
+		#The service will receive position request from clients
 		rospy.Service('/'+self.name+'/zumy_tracking', Mov2LocSrv, self.move)
 		print self.name+' is alive'
 
+	#updatePermission will be called to update moveEnable whenever a new message is received from the topic
+	def updatePermission(self, msg):
+		self.moveEnable = msg.data
+
+	# Just keep running the node
 	def run(self):
 		print self.name+ " is running"
 		print self.goal
 		rospy.spin()
+	#getPos will be called to update zumy's position when receive a new message from topic
 	def getPos(self, msg):
 		self.position = msg.position
 		#print self.position
 
-	# def pubTFPos(self):
-	# 	counter = 0
-	# 	state = Transform()
-	# 	state.translation.x = self.position.x
-	# 	state.translation.y = self.position.y
-	# 	state.rotation.z = np.sin(self.position.theta/2.)
-	# 	state.rotation.w = np.cos(self.position.theta/2.) # Quaternion form
-	# 	self.state_pub.publish(state)
-
-	# 	state_tf = TFMessage()
-	# 	state_tf.transforms = [TransformStamped()]
-	# 	state_tf.transforms[0].header.seq = counter
-	# 	state_tf.transforms[0].header.frame_id = "ar_marker_0"
-	# 	#print self.origin_tag
-	# 	state_tf.transforms[0].child_frame_id = self.name
-	# 	state_tf.transforms[0].transform = state
-	# 	self.pos_tf_pub.publish(state_tf)
-
-
-
 	def move(self, request):
-			#Set rospy rate
-		#Expose variables from subscriber line
+
 		self.goal = request.goal
 		self.goal_flag = False
-		self.counter = 0
+		self.goalCounter = 0
 		
 		cmd = Twist()	
 		cmd.linear.y = 0
@@ -69,23 +63,25 @@ class MoveZumy:
 
 		#Creating a new current state based on the information from Haoyu's code		
 		#Plugging the information from Haoyu's code into Vijay's getCmdVel function to calculate v_x and omega_z
-		while self.counter<5:
-			#self.pubTFPos()
-
+		while self.goalCounter<5:
 			(vel, self.goal_flag) = get_vel.getCmdVel(self.position, self.goal)
 
 		#Creating the ability to publish to the zumy
 
 		#Creating the message type to publish to the zumy (information from vel)
 
-			if not self.goal_flag:
+			if (not self.goal_flag) and self.moveEnable:
 				cmd.angular.z = vel['ang_z']
 				cmd.linear.x = vel['lin_x']
-				self.counter = 0
-			else:
+				self.goalCounter = 0
+			if self.goal_flag and self.moveEnable:
 				cmd.angular.z = 0
 				cmd.linear.x = 0
-				self.counter = self.counter + 1
+				self.goalCounter = self.goalCounter + 1
+			if not self.moveEnable:
+				cmd.angular.z = 0
+				cmd.linear.x = 0
+				self.goalCounter = 0				
 
 			#Publish new velocity information to the zumy
 			self.vel_pub.publish(cmd)
@@ -103,32 +99,3 @@ if __name__=='__main__':
 		sys.exit()
 	little_zumy = MoveZumy(sys.argv[1])
 	little_zumy.run()
-
-
-	
-	#rospy.Service('zumy_location', Mov2LocSrv, move)
-	# print "Ready to receive cmds to move Zumys"
-	# goal_state = {'x': 0.5, 'y': 0.5, 'theta': 0}
-	# zumy = sys.argv[1]
-	# 
-	# s = rospy.Subscriber('zumy_position', ZumyCoord, move, [goal_state, zumy])
-	# print s._connection_header
-	# rate.sleep()
-	#rospy.spin()
-
-
-	# for zumy_name in sys.argv[1:]:
-	# 	#print zumy_name
-	# 	zumynode[zumy_name] =  MoveZumy(zumy_name)
-	# 	zumynode[zumy_name].run()
-
-	#Initiates our node
-	
-	
-	# #Defines parameters that ultimately get fed to our move function
-	# 
-	# 
-
-	# #Subscribes to the zumy_position topic created by Haoyu's code
-	# rospy.Subscriber('zumy_position', ZumyCoord, move, [goal_state, zumy])
-	# rospy.spin()
